0ed2154d8919b3a740f2682c66f4f976e09c4339
gokul <asdf1234@>
2021-04-14 01:41:19 +0530

    Updates

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php 
+// This exists purely to satisfy Heroku's requirements that an index.php file exists in the root directory.
+// It loads the real index file.
+
+include_once("index.html"); 
+?>
